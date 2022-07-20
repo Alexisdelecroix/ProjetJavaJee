4ba97321279caff4365255040cb70638d1768345
59013-36-15 <d-alexis@hotmail.fr>
2022-07-20 15:20:09 +0200

    [Ajout de la 2 ème vue]

--- src/main/java/com/example/demo/ProjetJavaJeeApplication.java
+++ src/main/java/com/example/demo/ProjetJavaJeeApplication.java
@@ -24,7 +24,56 @@ ApplicationContext ctx=SpringApplication.run(ProjetJavaJeeApplication.class, arg
 	produitRepository.save(new Produit("Ecran 4K", 800, 100));
 	produitRepository.save(new Produit("Drône", 399, 50));
 	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
-	
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
+	produitRepository.save(new Produit("Xiaomi S Pro", 1000, 500));
+	produitRepository.save(new Produit("Ordinateur portable", 1500, 80));
+	produitRepository.save(new Produit("Ecran 4K", 800, 100));
+	produitRepository.save(new Produit("Drône", 399, 50));
+	produitRepository.save(new Produit("Tour Gaming", 2000, 30));
 	
 	produitRepository.findAll().forEach(p->System.out.println(p.getDesignation()));*/
 	}
--- src/main/java/com/example/demo/web/ProduitController.java
+++ src/main/java/com/example/demo/web/ProduitController.java
@@ -1,14 +1,12 @@
 package com.example.demo.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dao.ProduitRepository;
@@ -19,24 +17,37 @@ public class ProduitController {
 	@Autowired
 	private ProduitRepository produitRepository;
 
-@RequestMapping(value="/index")
-public String index(Model model,
-		@RequestParam(name="page", defaultValue="0")int p,
-		@RequestParam(name="size", defaultValue="10")int s){
-	Pageable pageable = PageRequest.of(p,s);
+	@RequestMapping(value="/index")
+	public String index(Model model,
+	        @RequestParam(name="page", defaultValue="0")int p,
+	        @RequestParam(name="size", defaultValue="5")int s,
+	        @RequestParam(name="motCle", defaultValue="")String mc){
+	    Page<Produit> pageProduits = produitRepository.chercher("%"+mc+"%",PageRequest.of(p, s));
+	    
+	    model.addAttribute("listProduits",pageProduits.getContent());
+	    
+	    int[] pages= new int[pageProduits.getTotalPages()];
+	    model.addAttribute("pages", pages);
+	    model.addAttribute("size", s);
+	    model.addAttribute("pageCourante", p);
+	    model.addAttribute("motCle", mc);
+	    return "produits";
 
-	Page<Produit> produits = produitRepository.findAll(pageable);
-	model.addAttribute("listProduits",produits.getContent());
-
-	Page<Produit> pageProduits = null;
-	int[] pages=new int[pageProduits.getTotalPages()];
-	model.addAttribute("pages",pages);
-	return "produits";
-
+	    }
+	@RequestMapping(value="/delete",method=RequestMethod.GET)
+	public String delete(Long id,String motCle,int page, int size) {
+		produitRepository.deleteById(id);
+		return "redirect:/index?page="+page+"&size="+size+"&motCle="+motCle;
+	}
+	
+	@RequestMapping(value="/form",method=RequestMethod.GET)
+	public String formProduit() {
+		return "FormProduit";
+	}
 }
 	
 		
 	
 	
 
-}
+
