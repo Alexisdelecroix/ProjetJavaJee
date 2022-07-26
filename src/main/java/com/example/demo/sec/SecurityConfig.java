c809b369d6351da40b55e1fa3c6efb15df210ab5
59013-36-15 <d-alexis@hotmail.fr>
2022-07-26 11:34:20 +0200

    Définition des rôles, admin et user

--- src/main/java/com/example/demo/sec/SecurityConfig.java
+++ src/main/java/com/example/demo/sec/SecurityConfig.java
@@ -1,30 +1,47 @@
 package com.example.demo.sec;
 
 
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.Md4PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Autowired 
+	private DataSource dataSource;
 @Override
 protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+	/*
 	auth.inMemoryAuthentication()
 		.withUser("admin").password("{noop}1234").roles("USER", "ADMIN");
 	auth.inMemoryAuthentication()
 	.withUser("user").password("{noop}1234").roles("USER");
-	
+	*/
 
+	auth.jdbcAuthentication()
+	.dataSource((javax.sql.DataSource) dataSource)
+	.usersByUsernameQuery("select login as principal ,pass as credentials,active from users where login=?")
+	.authoritiesByUsernameQuery("select login as principal, role as role from users_role where login=?")
+	.passwordEncoder(NoOpPasswordEncoder.getInstance())
+	.rolePrefix("ROLE_");
 
 }
 
 @Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin();
-		http.authorizeRequests().antMatchers("/index").hasRole("USER");
-		http.authorizeRequests().antMatchers("/form", "/save", "/edit","/delete").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers("/user/*").hasRole("USER");
+		http.authorizeRequests().antMatchers("/admin/*").hasRole("ADMIN");
 		http.exceptionHandling().accessDeniedPage("/403");
 	}
 }
